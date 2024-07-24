3add42a8afe9fabcda74943f27d4a7672d4c2636
youngishcord <kulikobyura@mail.ru>
2024-07-25 00:55:39 +0300

    еще файлы из примера

---
+++ config/boards/shields/test_zmk/widgets/modifiers.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <dt-bindings/zmk/modifiers.h>
+#include <lvgl.h>
+#include <zephyr/kernel.h>
+
+#define SIZE_SYMBOLS 14 // 14 x 14 pixel
+
+struct zmk_widget_modifiers {
+  sys_snode_t node;
+  lv_obj_t *obj;
+};
+
+int zmk_widget_modifiers_init(struct zmk_widget_modifiers *widget,
+                              lv_obj_t *parent);
+lv_obj_t *zmk_widget_modifiers_obj(struct zmk_widget_modifiers *widget);
